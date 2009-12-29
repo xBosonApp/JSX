f1c7c53c3d37fd16a93710ff97772ed34ba27eb5
CatfoOD <yanming-sohu@sohu.com>
2009-12-29 21:28:09 +0800

    Signed-off-by: CatfoOD <yanming-sohu@sohu.com>

--- common.js
+++ common.js
@@ -1,5 +1,5 @@
 // CatfoOD 2009.12.9
-// v0.15
+// v0.16
 
 
 /**
@@ -13,7 +13,7 @@ function waitTag(alertfunc, id) {
 		if (getByid(id)===null) {
 			setTimeout(func, 100);
 		} else {
-			if (typeof alertfunc=='function') {
+			if (typeof alertfunc==='function') {
 				alertfunc();
 			}
 			else {
@@ -22,6 +22,22 @@ function waitTag(alertfunc, id) {
 		}
 	}
 	func();
+}
+
+function waitBody(alertfunc) {
+	var f = function() {
+		if (!document.body) {
+			setTimeout(f, 100);
+		} else {
+			if (typeof alertfunc==='function') {
+				alertfunc();
+			}
+			else {
+				eval(alertfunc);
+			}
+		}
+	}
+	f();
 }
 
 /**
@@ -44,7 +60,11 @@ function moveCenter(divid) {
 
 
 function getByid(id) {
-	return document.getElementById(id);
+	try {
+		return document.getElementById(id);
+	} catch(e) {
+		return null;
+	}
 }
 
 /**
@@ -52,7 +72,7 @@ function getByid(id) {
  */
 function showError(msg) {
 	window.status = msg;
-	var ediv = document.createElement("p");
+	var ediv = document.createElement("div");
 	ediv.style.color = "#FF0000";
 	ediv.style.fontSize = "11px";
 	ediv.innerText = msg;
@@ -68,7 +88,10 @@ function showError(msg) {
  */
 function getFormData(formid) {
 	var formtext = new Array();
-	var form = getByid(formid);;
+	var form = getByid(formid);
+	if (!form) {
+		form = formid;
+	}
 	
 	var pushvalue = function(name, value, last) {
 		formtext.push( name );
@@ -186,7 +209,7 @@ function encodeUri(uri) {
 
 function getDiv(divid) {
 	var div = null;
-	if (typeof divid == "string") {
+	if (typeof divid === "string") {
 		div = getByid(divid);
 	} else {
 		div = divid;
@@ -199,6 +222,19 @@ function getDiv(divid) {
  */
 function showDiv(divid) {
 	var div = getDiv(divid);
+	var alpha = 0;
+	var strength = 20;
+	
+	var func = function() {
+		if (alpha<90) {
+			div.style.filter = "progid:DXImageTransform.Microsoft.Alpha(opacity="+alpha+") "
+					+ "progid:DXImageTransform.Microsoft.MotionBlur(Strength="+strength+",Direction=200);";
+			setTimeout(func, 20);
+			alpha += 8;
+			strength-=2;
+		}
+	}
+	func();
 	div.style.display = "block";
 }
 
@@ -207,7 +243,21 @@ function showDiv(divid) {
  */
 function hideDiv(divid) {
 	var div = getDiv(divid);
-	div.style.display = "none";
+	var alpha = 90;
+	var strength = 0;
+	
+	var func = function() {
+		if (alpha>0) {
+			div.style.filter = "progid:DXImageTransform.Microsoft.Alpha(opacity="+alpha+") "
+					+ "progid:DXImageTransform.Microsoft.MotionBlur(Strength="+strength+",Direction=35);";
+			setTimeout(func, 20);
+			alpha -= 8;
+			strength+=2;
+		} else {
+			div.style.display = "none";
+		}
+	}
+	func();
 }
 
 /**
@@ -216,4 +266,302 @@ function hideDiv(divid) {
 function divDisplay(divid) {
 	var div = getDiv(divid);
 	return div.style.display == "block";
+}
+
+
+/**
+ * 修改tableid指定的表的颜色，其颜色为隔行显示
+ * 
+ * @param tableid - 表格的id
+ * @param fcolor - 奇数行的颜色
+ * @param scolor - 偶数行的颜色
+ * @param mousecolor - 鼠标悬停的颜色
+ * @return 返回tableid的对象
+ */
+function changeTableColor(tableid, fcolor, scolor, mousecolor) {
+	var table = getByid(tableid);
+	if (table==null) return;
+	
+	var color = true;
+	
+	if (fcolor==null) {
+		fcolor = '#dddddd';
+	}
+	if (scolor==null) {
+		scolor = '#f0f0f0';
+	}
+	if (mousecolor==null) {
+		mousecolor = '#ffaaaa';
+	}
+	
+	var rows = table.rows;
+	for (var i=0; i<rows.length; ++i) {
+		var row = rows[i];
+		
+		if (color) {
+			changeColor(row, fcolor);
+		} else {
+			changeColor(row, scolor);
+		}
+		color = !color;
+
+		onMouseOverChangeColor(row, mousecolor);
+	}
+	
+	return table;
+}
+
+/**
+ * 表格行鼠标点击事件
+ * 
+ * @param tableid - 要监听的表格id
+ * @param func - 监听函数,第一个参数为当前鼠标悬停的tr对象,
+ * 						第二个参数是当前行的索引从0开始
+ * @return 返回tableid的对象
+ */
+function tableRowMouseOverListener(tableid, func) {
+	var table = getByid(tableid);
+	if (table==null) return;
+	
+	var rows = table.rows;
+	for (var i=0; i<rows.length; ++i) {
+		(function () {
+			var funcstr = func;
+			var row = rows[i];
+			var rowindex = i;
+			
+			row.ondblclick = function () {
+				funcstr(row, rowindex);
+			}
+		})();
+	}
+	
+	return table;
+}
+
+/**
+ * 当鼠标悬停在obj标记上时，颜色变为color
+ * 
+ * @param obj - html标记对象
+ * @param color - 有效的css颜色值
+ * @return null
+ */
+function onMouseOverChangeColor(obj, color) {
+	var oldcolor = getColorInt(obj.style.backgroundColor);
+	var ncolor = getColorInt(color);
+	
+	var lock = new LockObj(obj);
+	
+	obj.onmouseover = function() {
+		if (oldcolor) {
+			transitionColor(obj, oldcolor, ncolor);
+		} else {
+			changeColor(obj, ncolor);
+		}
+	}
+	obj.onmouseout = function() {
+		if (oldcolor) {
+			transitionColor(obj, ncolor, oldcolor);
+		} else {
+			changeColor(obj, oldcolor);
+		}
+	}
+}
+
+/**
+ * obj的颜色由scolor变为ecolor, 颜色值为整数 
+ */
+function transitionColor(obj, scolor, ecolor) {
+	var count = 15;
+	
+	var er = ecolor >>> 16;
+	var eg =(ecolor & 0x00ff00) >>> 8;
+	var eb =(ecolor & 0x0000ff);
+	
+	var sr = scolor >>> 16;
+	var sg =(scolor & 0x00ff00) >>> 8;
+	var sb =(scolor & 0x0000ff);
+	
+	var rstep = (er-sr)/count;
+	var gstep = (eg-sg)/count;
+	var bstep = (eb-sb)/count;
+	
+	var cc = 0;
+	var i = 0;
+	
+	var func = function() {
+		if ( i<count ) {
+			sr += rstep;
+			sg += gstep;
+			sb += bstep;
+			cc = (sr<<16) | (sg<<8) | (sb);
+			changeColor(obj, cc);
+			
+			setTimeout(func, 20);
+			i++;
+		}
+	}
+	func();
+}
+
+/**
+ * 线程锁对象，在obj对象上添加锁
+ */
+function LockObj(obj) {
+	var lockname = "jym_jsx_thread_lock_object";
+	
+	if ( !obj[lockname] ) {
+		obj[lockname] = 0;
+	}
+	
+	/**
+	 * 在对象上加锁，如果对象已经被其他线程锁定，
+	 * 则需要等待，锁被释放才能继续执行
+	 *
+	 * @parm func - 锁被释放后，执行的方法
+	 */
+	this.lock = function(func) {
+		if (typeof func==='function') {
+			var waitlock = function() {
+				if (obj[lockname]>0) {
+					setTimeout(waitlock, 50);
+				} else {
+					obj[lockname]++;
+					func();
+				}
+			}
+			waitlock();
+		}
+	}
+	
+	/**
+	 * 如果对象没有被锁定，执行func函数
+	 * 否则忽略
+	 */
+	this.check = function(func) {
+		if (typeof func==='function') {
+			if (obj[lockname]==0) {
+				obj[lockname]++;
+				func();
+			}
+		}
+	}
+	
+	/**
+	 * 释放对象上的锁
+	 */
+	this.free = function() {
+		obj[lockname]--;
+		if ( obj[lockname]<0 ) obj[lockname]=0;
+	}
+}
+
+/**
+ * css颜色格式('#000000')转换为整数，并返回
+ */
+function getColorInt(csscolor) {
+	if (csscolor) {
+		return parseInt( csscolor.substr(1,csscolor.length),16 );
+	} else {
+		return false; //0xffffff;
+	}
+}
+
+/**
+ * 修改obj的颜色为color
+ * 
+ * @param obj - html标记对象
+ * @param color - 有效的css颜色值, 如果color为整数自动格式化为css格式
+ *					如果color===false, 则对象变为透明背景
+ * @return null
+ */
+function changeColor(obj, color) {
+	if (color===false) {
+		obj.style.backgroundColor = '';
+	}
+	else if (isNaN(obj)) {
+		obj.style.backgroundColor = color;
+	} else {
+		var c = new Number(color).toString(16);
+		obj.style.backgroundColor = '#'+c;
+	}
+}
+
+
+/**
+ * 对div标签的封装类，让divid指定的div可以被鼠标拖动
+ * @author CatfoOD 2009
+ * 
+ * @param divid - 目标divid
+ * @param touchid - 触发鼠标事件的对象的id,如果为null,则div不可用鼠标拖动
+ * 					如果touchid不为null并且指定的对象无效，
+ * 					则使用div自己触发鼠标拖动事件
+ */
+function DivPack(divid, touchid) {
+	var div = document.getElementById(divid);
+
+	var x = 0;
+	var y = 0;
+	var stop = true;
+	var mx = 0;
+	var my = 0;
+	var ins = this;
+	
+	this.setX = function(nx) {
+		x = nx;
+		div.style.left = x;
+	}
+	
+	this.setY = function(ny) {
+		y = ny;
+		div.style.top = y;
+	}
+	
+	this.getX = function() {
+		return x;
+	}
+	
+	this.getY = function() {
+		return y;
+	}
+	
+	this.getDiv = function() {
+		return div;
+	}
+	
+	var divmove = function() {
+		stop = false;
+		mx = event.screenX;
+		my = event.screenY;
+	}
+
+	var cancelmove = function() {
+		stop = true;
+	}
+
+	var mousemove = function () {
+		if (stop) return;
+		
+		var offx = event.screenX;
+		var offy = event.screenY;
+		
+		ins.setX( ins.getX()+offx-mx );
+		ins.setY( ins.getY()+offy-my );
+
+		mx = offx;
+		my = offy;
+	}
+	
+	if (touchid) {
+		var toucher = document.getElementById(touchid);
+		if (toucher==null) {
+			toucher = div;
+		}
+		toucher.onmousedown = divmove;
+		toucher.onmousemove = mousemove;
+		toucher.onmouseover = cancelmove;
+		toucher.onmouseup = cancelmove;
+		
+		div.style.position = "absolute";
+	}
 }
